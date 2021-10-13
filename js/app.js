733a9cda4ae7707d3ef9b38910f379330686d891
nndeto <86444679+nndeto@users.noreply.github.com>
2021-10-13 00:13:31 -0400

    added variables

--- js/app.js
+++ js/app.js
@@ -5,4 +5,12 @@ const wandererImage = document.getElementById("wanderer-img");
 const rooImage = document.getElementById("roo-img")
 
 //img sources
-const tSOne = "images/project-images/tamagotchi-landing.png"
+const tSOne = "images/project-images/tamagotchi-landing.png";
+const tSTwo = "images/project-images/tamagotchi-gameplay.png";
+const jSOne = "images/project-images/journ.ey-landing.png";
+const jSTwo = "images/project-images/journ-ey-all.png";
+const wSOne = "images/project-images/wanderer-landing.png";
+const wSTwo = "images/project-images/wanderer-about.png";
+const rSOne = "images/project-images/roo-landing.png";
+const rSTwo = "images/project-images/roo-roommate.png";
+
