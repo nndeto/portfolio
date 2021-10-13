1f0f137e4054892fe17e980efa842368440f8521
nndeto <86444679+nndeto@users.noreply.github.com>
2021-10-13 00:09:19 -0400

    cut pictures from html

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//global variables
+const tamagotchiImage = document.getElementById("tamagotchi-img");
+const journeyImage = document.getElementById("journey-img");
+const wandererImage = document.getElementById("wanderer-img");
+const rooImage = document.getElementById("roo-img")
+
+//img sources
+const tSOne = "images/project-images/tamagotchi-landing.png"
