1f6bb7766062b4a3cf71e2ab41137b452a60599a
nndeto <86444679+nndeto@users.noreply.github.com>
2021-10-13 00:20:36 -0400

    added image change events

--- js/app.js
+++ js/app.js
@@ -14,3 +14,44 @@ const wSTwo = "images/project-images/wanderer-about.png";
 const rSOne = "images/project-images/roo-landing.png";
 const rSTwo = "images/project-images/roo-roommate.png";
 
+// image changes
+function tChangeOne() {
+    tamagotchiImage.src = tSTwo;
+}
+function tChangeTwo() {
+    tamagotchiImage.src = tSOne;
+}
+
+function jChangeOne() {
+    journeyImage.src = jSTwo;
+}
+function jChangeTwo() {
+    journeyImage.src = jSOne;
+}
+
+function wChangeOne() {
+    wandererImage.src = wSTwo;
+}
+function wChangeTwo() {
+    wandererImage.src = wSOne;
+}
+
+function rChangeOne() {
+    rooImage.src = rSTwo;
+}
+function rChangeTwo() {
+    rooImage.src = rSOne;
+}
+
+//image event listeners
+tamagotchiImage.addEventListener("mouseover", tChangeOne)
+tamagotchiImage.addEventListener("mouseout", tChangeTwo)
+
+journeyImage.addEventListener("mouseover", jChangeOne)
+journeyImage.addEventListener("mouseout", jChangeTwo)
+
+wandererImage.addEventListener("mouseover", wChangeOne)
+wandererImage.addEventListener("mouseout", wChangeTwo)
+
+rooImage.addEventListener("mouseover", rChangeOne)
+rooImage.addEventListener("mouseout", rChangeTwo)
