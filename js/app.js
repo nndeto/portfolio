9ded43e9e18b6c450203f5fa0d0eb58a55dedcca
nndeto <86444679+nndeto@users.noreply.github.com>
2021-10-29 01:14:07 -0400

    converted images to webp to improve performance

--- js/app.js
+++ js/app.js
@@ -5,14 +5,14 @@ const wandererImage = document.getElementById("wanderer-img");
 const rooImage = document.getElementById("roo-img")
 
 //img sources
-const tSOne = "images/project-images/tamagotchi-landing.png";
-const tSTwo = "images/project-images/tamagotchi-gameplay.png";
-const jSOne = "images/project-images/journ.ey-landing.png";
-const jSTwo = "images/project-images/journ-ey-all.png";
-const wSOne = "images/project-images/wanderer-landing.png";
-const wSTwo = "images/project-images/wanderer-about.png";
-const rSOne = "images/project-images/roo-landing.png";
-const rSTwo = "images/project-images/roo-roommate.png";
+const tSOne = "images/project-images/tamagotchi-landing.webp";
+const tSTwo = "images/project-images/tamagotchi-gameplay.webp";
+const jSOne = "images/project-images/journ.ey-landing.webp";
+const jSTwo = "images/project-images/journ-ey-all.webp";
+const wSOne = "images/project-images/wanderer-landing.webp";
+const wSTwo = "images/project-images/wanderer-about.webp";
+const rSOne = "images/project-images/roo-landing.webp";
+const rSTwo = "images/project-images/roo-roommate.webp";
 
 // image changes
 function tChangeOne() {
